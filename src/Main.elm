7bab51aaf1d399b82c36c487b5d0d8bccfb630c1
Xander <info@web-refinery.com>
2021-08-13 22:28:20 +0200

    Scrolling changes color

--- src/Main.elm
+++ src/Main.elm
@@ -4,11 +4,16 @@ import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onMouseOver)
-import Json.Encode
+import Html.Events.Extra.Wheel as Wheel
+
+
+
+-- Some green: red = 25, green = 200, blue = 110
 
 
 type Msg
     = ColorCell Int
+    | ChangeColor Wheel.Event
 
 
 type alias RGB =
@@ -20,43 +25,52 @@ type alias Cell =
 
 
 type alias Model =
-    { color : RGB, cells : List Cell }
+    { color : RGB, previousColor : RGB, cells : List Cell, colors : List RGB }
+
+
+colorList : List RGB
+colorList =
+    [ { red = 50, green = 50, blue = 50 } -- gray
+    , { red = 252, green = 186, blue = 3 } -- orange
+    , { red = 252, green = 231, blue = 3 } -- yellow
+    , { red = 1, green = 99, blue = 255 } -- blue
+    , { red = 25, green = 200, blue = 110 } -- green
+    , { red = 200, green = 0, blue = 50 } -- red
+    , { red = 255, green = 255, blue = 255 } -- white
+    ]
 
 
 initialModel : Model
 initialModel =
     { color =
         { red = 50, green = 50, blue = 50 }
-    , cells = createCells (24 * 24) []
+    , previousColor = { red = 255, green = 255, blue = 255 }
+    , cells = createCells (24 ^ 2) []
+    , colors = colorList
     }
 
 
 view : Model -> Html Msg
 view model =
-    div [ class "container" ]
-        [ div [ class "pure-g" ] (List.map viewCell model.cells)
+    div
+        [ id "grid"
+        , style "border-color" (getRgb model.color)
+        , Wheel.onWheel ChangeColor
         ]
+        (List.map viewCell model.cells)
 
 
-
---(List.map viewCell model.cells)
+getRgb : RGB -> String
+getRgb color =
+    "rgb("
+        ++ String.join "," [ String.fromInt (.red color), String.fromInt (.green color), String.fromInt (.blue color) ]
+        ++ ")"
 
 
 viewCell : Cell -> Html Msg
 viewCell cell =
-    let
-        rgbStyle =
-            "rgb("
-                ++ String.fromInt (.red cell.color)
-                ++ ","
-                ++ String.fromInt (.green cell.color)
-                ++ ","
-                ++ String.fromInt (.blue cell.color)
-                ++ ")"
-    in
     div
-        [ class "pure-u-1-24 cell"
-        , style "background-color" rgbStyle
+        [ style "background-color" (getRgb cell.color)
         , onMouseOver (ColorCell cell.id)
         ]
         [ text "" ]
@@ -78,6 +92,49 @@ createCells n cells =
 update : Msg -> Model -> Model
 update msg model =
     case msg of
+        ChangeColor wheel ->
+            let
+                findNext n =
+                    case n of
+                        [] ->
+                            Nothing
+
+                        x :: [] ->
+                            if x == model.color then
+                                List.head model.colors
+
+                            else
+                                Nothing
+
+                        x :: y :: rest ->
+                            if x == model.color then
+                                Just y
+
+                            else
+                                findNext (y :: rest)
+
+                listOfColors =
+                    if wheel.deltaY > 0 then
+                        List.reverse model.colors
+
+                    else
+                        model.colors
+
+                newColor =
+                    if wheel.deltaY > 0 then
+                        model.previousColor
+
+                    else
+                        Maybe.withDefault model.color (findNext model.colors)
+            in
+            { model | color = newColor, previousColor = model.color }
+
+        --List.map (\color ->
+        --    if color = model.color then
+        --
+        --)
+        --in
+        --{ model | color = { red = 1, green = 99, blue = 255 } }
         ColorCell index ->
             let
                 newCells =
