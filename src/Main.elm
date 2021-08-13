a42974f46096a9fe9e0b22c93b8b868268d71240
Xander <info@web-refinery.com>
2021-08-13 13:09:16 +0200

    initial sketchin' app

--- src/Main.elm
+++ src/Main.elm
@@ -1,160 +1,111 @@
-port module Main exposing (Model, Msg(..), add1, init, main, toJs, update, view)
+module Main exposing (..)
 
 import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onClick)
-import Http exposing (Error(..))
-import Json.Decode as Decode
-
-
-
--- ---------------------------
--- PORTS
--- ---------------------------
-
-
-port toJs : String -> Cmd msg
-
-
-
--- ---------------------------
--- MODEL
--- ---------------------------
-
-
-type alias Model =
-    { counter : Int
-    , serverMessage : String
-    }
-
-
-init : Int -> ( Model, Cmd Msg )
-init flags =
-    ( { counter = flags, serverMessage = "" }, Cmd.none )
-
-
-
--- ---------------------------
--- UPDATE
--- ---------------------------
+import Html.Events exposing (onMouseOver)
+import Json.Encode
 
 
 type Msg
-    = Inc
-    | TestServer
-    | OnServerResponse (Result Http.Error String)
+    = ColorCell Int
 
 
-update : Msg -> Model -> ( Model, Cmd Msg )
-update message model =
-    case message of
-        Inc ->
-            ( add1 model, toJs "Inc" )
-
-        TestServer ->
-            let
-                expect =
-                    Http.expectJson OnServerResponse (Decode.field "result" Decode.string)
-            in
-            ( model
-            , Http.get { url = "/test", expect = expect }
-            )
-
-        OnServerResponse res ->
-            case res of
-                Ok r ->
-                    ( { model | serverMessage = r }, Cmd.none )
-
-                Err err ->
-                    ( { model | serverMessage = "Error: " ++ httpErrorToString err }, Cmd.none )
+type alias RGB =
+    { red : Int, green : Int, blue : Int }
 
 
-httpErrorToString : Http.Error -> String
-httpErrorToString err =
-    case err of
-        BadUrl url ->
-            "BadUrl: " ++ url
-
-        Timeout ->
-            "Timeout"
-
-        NetworkError ->
-            "NetworkError"
-
-        BadStatus _ ->
-            "BadStatus"
-
-        BadBody s ->
-            "BadBody: " ++ s
+type alias Cell =
+    { id : Int, color : RGB }
 
 
-{-| increments the counter
-
-    add1 5 --> 6
-
--}
-add1 : Model -> Model
-add1 model =
-    { model | counter = model.counter + 1 }
+type alias Model =
+    { color : RGB, cells : List Cell }
 
 
-
--- ---------------------------
--- VIEW
--- ---------------------------
+initialModel : Model
+initialModel =
+    { color =
+        { red = 50, green = 50, blue = 50 }
+    , cells = createCells (24 * 24) []
+    }
 
 
 view : Model -> Html Msg
 view model =
     div [ class "container" ]
-        [ header [ class "pure-g" ]
-            [ span [ class "pure-u-1-5" ] [ img [ src "/images/logo.png" ] [] ]
-            , h1 [ class "pure-u-4-5" ] [ text "Elm 0.19.1 Webpack Starter, with hot-reloading" ]
-            ]
-        , p [] [ text "Click on the button below to increment the state." ]
-        , div [ class "pure-g" ]
-            [ div [ class "pure-u-1-3" ]
-                [ button
-                    [ class "pure-button pure-button-primary"
-                    , onClick Inc
-                    ]
-                    [ text "+ 1" ]
-                , text <| String.fromInt model.counter
-                ]
-            , div [ class "pure-u-1-3" ] []
-            , div [ class "pure-u-1-3" ]
-                [ button
-                    [ class "pure-button pure-button-primary"
-                    , onClick TestServer
-                    ]
-                    [ text "ping dev server" ]
-                , text model.serverMessage
-                ]
-            ]
-        , p [] [ text "Then make a change to the source code and see how the state is retained after recompilation." ]
-        , p []
-            [ text "And now don't forget to add a star to the Github repo "
-            , a [ href "https://github.com/simonh1000/elm-webpack-starter" ] [ text "elm-webpack-starter" ]
-            ]
+        [ div [ class "pure-g" ] (List.map viewCell model.cells)
         ]
 
 
 
--- ---------------------------
--- MAIN
--- ---------------------------
+--(List.map viewCell model.cells)
 
 
-main : Program Int Model Msg
+viewCell : Cell -> Html Msg
+viewCell cell =
+    let
+        rgbStyle =
+            "rgb("
+                ++ String.fromInt (.red cell.color)
+                ++ ","
+                ++ String.fromInt (.green cell.color)
+                ++ ","
+                ++ String.fromInt (.blue cell.color)
+                ++ ")"
+    in
+    div
+        [ class "pure-u-1-24 cell"
+        , style "background-color" rgbStyle
+        , onMouseOver (ColorCell cell.id)
+        ]
+        [ text "" ]
+
+
+createCells n cells =
+    let
+        newCells =
+            { id = n, color = { red = 255, green = 255, blue = 255 } } :: cells
+    in
+    case n of
+        1 ->
+            newCells
+
+        _ ->
+            createCells (n - 1) newCells
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        ColorCell index ->
+            let
+                newCells =
+                    List.map
+                        (\cell ->
+                            if cell.id == index then
+                                { cell | color = model.color }
+
+                            else
+                                cell
+                        )
+                        model.cells
+            in
+            { model | cells = newCells }
+
+
+main : Program () Model Msg
 main =
-    Browser.document
-        { init = init
+    Browser.sandbox
+        { init = initialModel
+        , view = view
         , update = update
-        , view =
-            \m ->
-                { title = "Elm 0.19 starter"
-                , body = [ view m ]
-                }
-        , subscriptions = \_ -> Sub.none
         }
+
+
+
+--
+--main =
+--    text ""
+--
+--
--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@
 require("./styles.scss");
 
 const {Elm} = require('./Main');
-var app = Elm.Main.init({flags: 6});
+var app = Elm.Main.init({node: document.getElementById("app")});
+// var app = Elm.Main.init({flags: 6});
 
-app.ports.toJs.subscribe(data => {
-    console.log(data);
-})
+// app.ports.toJs.subscribe(data => {
+//     console.log(data);
+// })
 // Use ES2015 syntax and let Babel compile it for you
 var testFn = (inp) => {
     let a = inp + 1;
